ca358b1baa17c974e014bf70130bb3d79fd89914
Sanchez <sanchez.cliff.86@gmail.com>
2021-09-06 09:54:04 -0700

    upload, tailwind, styled

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>main content</main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
