99640139ce0fe388cb438735b2fe598a560b5fd5
cxs2549 <sanchez.cliff.86@gmail.com>
2021-09-07 11:20:27 -0700

    add features, ship

--- src/views/Home/Hero/Hero.js
+++ src/views/Home/Hero/Hero.js
@@ -1,62 +1,68 @@
-import styled from 'styled-components'
-import hero from '../../../assets/hero.jpg'
+import styled from "styled-components";
+import hero from "../../../assets/hero.jpg";
 
 const StyledHero = styled.div`
-	height: 64vh;
-	width: 100vw;
-	background-image: url(${hero});
-	background-position: 0px center;
-    background-position: 0 0;
-    background-repeat: no-repeat;
-	background-size: cover;
-	color: white;
-	position: relative;
- 
-    @media (min-width: 1280px) {
-        height: 70vh;
-        background-position: 0px -50px;
-    }
-	&::after {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background-image: linear-gradient(to bottom, rgba(0, 0, 0, .3), transparent);
+  height: 64vh;
+  width: 100vw;
+  background-image: url(${hero});
+  background-position: center center;
+  background-position: -60px 0;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: white;
+  position: relative;
+	@media (min-width: 640px) {
+		background-position: 0 0;
 	}
-	h1, p {
-        color: white;
-		text-shadow: #000 1px 1px 3px;
-		position: relative;
-		z-index: 1;
-	}
-	span {
-		text-shadow: #fff 1px 1px 3px;
-
-	}
-`
+  @media (min-width: 1280px) {
+    height: 70vh;
+    background-position: 0px -160px;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.3),
+      transparent
+    );
+  }
+  h1,
+  p {
+    color: white;
+    text-shadow: #000 1px 1px 3px;
+    position: relative;
+    z-index: 1;
+  }
+  span {
+    text-shadow: #fff 1px 1px 3px;
+  }
+`;
 const Hero = () => {
-	return (
-		<div>
-			<StyledHero className="py-4">
-				<div className="mx-auto w-full max-w-6xl relative">
-					<div className="flex flex-col items-start justify-center absolute left-0 top-0 xl:top-4 px-4">
-                        <h1 className="text-4xl xl:text-5xl font-bold -mt-2 md:max-w-2xl">
-                            New my<span className="text-brandRed">6</span> Members get a Free Samsung
-                            Galaxy Smartphone
-                        </h1>
-                        <p className="mt-2 text-gray-700 relative z-10 xl:text-xl">
-                            When you pay for 2 months <br className="md:hidden" /> of wireless service.
-                        </p>
-                        <button className="px-4 py-2 mt-4 bg-brandBlue rounded text-white font-medium relative z-10 shadow xl:text-lg">
-                            Join Now
-                        </button>
-                    </div>
-				</div>
-			</StyledHero>
-
-		</div>
-	)
-}
-export default Hero
+  return (
+    <div>
+      <StyledHero className="py-4">
+        <div className="mx-auto w-full max-w-6xl relative">
+          <div className="flex flex-col items-start justify-center absolute left-0 top-0 xl:top-4 px-4">
+            <h1 className="text-4xl xl:text-5xl font-bold -mt-2 md:max-w-2xl">
+              New my<span className="text-brandRed">6</span> Members get a Free
+              Samsung Galaxy Smartphone
+            </h1>
+            <p className="mt-2 text-gray-700 relative z-10 xl:text-xl">
+              When you pay for 2 months <br className="md:hidden" /> of wireless
+              service.
+            </p>
+            <button className="px-4 py-2 mt-4 bg-brandBlue rounded text-white font-medium relative z-10 shadow xl:text-lg">
+              Join Now
+            </button>
+          </div>
+        </div>
+      </StyledHero>
+    </div>
+  );
+};
+export default Hero;
--- src/views/Home/Travel/Features/Features.js
+++ src/views/Home/Travel/Features/Features.js
@@ -1,10 +1,59 @@
-import styled from 'styled-components'
-const StyledFeatures = styled.div``
+import styled from "styled-components";
+import grand from "../../../../assets/grand.jpg";
+import hero from "../../../../assets/hero.jpg";
+import aarp from "../../../../assets/aarp.jpg";
+const StyledFeatures = styled.div`
+  span {
+    font-size: 1rem;
+  }
+  #features {
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    gap: .5rem;
+  }
+  h4 {
+    color: rgb(23, 107, 178);
+  }
+  p {
+    color: rgb(110, 118, 145);
+  }
+`;
 const Features = () => {
-return (
-<StyledFeatures className="p-4">
-features
-</StyledFeatures>
-)
-}
-export default Features
+  const feats = [
+    {
+      title: "The Grand Introduction",
+      desc: "Helping grandparents reunite with their grandkids",
+      img: grand,
+    },
+    {
+      title: "Samsung Smartphone Offer",
+      desc: "Sign up for My6 and Stay to be eligible",
+      img: hero,
+    },
+    { title: "AARP Members", desc: "AARP Members save even more", img: aarp },
+  ];
+  return (
+    <StyledFeatures className="p-4 py-6 flex relative justify-end">
+      <span className=" whitespace-nowrap transform -rotate-90 inline-block capitalize absolute -left-12 top-24 font-bold">
+        features and deals
+      </span>
+
+      <div  className="flex flex-col gap-2 w-full xl:border rounded justify-center py-2 xl:py-0">
+        {feats.map((feat, i) => (
+          <div id="features" key={i} className="flex  ml-12">
+            <img
+              src={feat.img}
+              className="w-24 h-24 object-top object-cover rounded"
+              alt=""
+            />
+            <div>
+              <h4 className='font-medium'>{feat.title}</h4>
+              <p className='text-sm'>{feat.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </StyledFeatures>
+  );
+};
+export default Features;
